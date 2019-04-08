3a442f070a88b09b847bd7a95d6925fb0144af7d
Jacob Yu <jacob.yu@DEV122.texturallc.net>
2019-04-08 10:29:26 -0500

    site skeleton with bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    publish_date = models.DateTimeField()
+    blog_body = models.TextField()
+
