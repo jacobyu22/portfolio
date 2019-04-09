d3fb51b7fd111735f27fe299c5a25c02e44e8a4c
Jacob Yu <jacob.yu@DEV122.texturallc.net>
2019-04-09 14:42:56 -0500

    sample portfolio site

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
     publish_date = models.DateTimeField()
     blog_body = models.TextField()
 
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.blog_body[:100]
+
+    def pub_date_short(self):
+        return self.publish_date.strftime('%b %e %Y')
